ba306842a78741f3f6b7f3dba79adb896c5339bc
DavidNebot94 <informaticadavidnebot@gmail.com>
2022-11-24 11:08:04 +0100

    David Nebot apply changes

---
+++ src/Exercise19.java
@@ -0,0 +1,20 @@
+public class Exercise19 {
+    private int x;
+    private int y;
+    public Exercise8(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Exercise19() {
+        this(0, 0);
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
