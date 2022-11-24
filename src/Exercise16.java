cf527683e03936033e1d072827a765110786ddb6
DavidNebot94 <informaticadavidnebot@gmail.com>
2022-11-24 10:47:05 +0100

    David Nebot apply changes

---
+++ src/Exercise16.java
@@ -0,0 +1,12 @@
+ public class Exercise16 {
+        private int x;
+        private int y;
+        public Exercise16(int a, int b) {
+            x = 0;
+            y = 0;
+        }
+        public Exercise16() {
+            x = 0;
+            y = 0;
+        }
+    }
