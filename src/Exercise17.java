6e620f69c0901114c57d69ae71295508616bc057
DavidNebot94 <informaticadavidnebot@gmail.com>
2022-11-24 10:52:30 +0100

    David Nebot apply changes

---
+++ src/Exercise17.java
@@ -0,0 +1,16 @@
+public class Exercise17 {
+        private int x;
+        private int y;
+        public Exercise17(int a, int b) {
+            x = a;
+            y = b;
+        }
+        public Exercise17() {
+            x = 0;
+            y = 0;
+        }
+        public void Constructor() {
+        x = 0;
+        y = 0;
+    }
+    }
