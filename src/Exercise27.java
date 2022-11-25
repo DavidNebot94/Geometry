44421c634c92139350dede76b22289585464edc3
DavidNebot94 <informaticadavidnebot@gmail.com>
2022-11-25 10:02:59 +0100

    David Nebot apply changes

---
+++ src/Exercise27.java
@@ -0,0 +1,51 @@
+public class Exercise27 {
+        private int a;
+        private int b;
+    private int c;
+    private int d;
+    private int e;
+    private int f;
+    private int g;
+    private int h;
+        public Exercise27(int a, int b   ) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.e = e;
+            this.f = f;
+            this.g = h;
+        }
+        // NO HE SABIDO COMO PLANTEAR EL EJERCICIO
+        public Exercise27() {
+            this(0, 0);
+        }
+        public void setA (int a) {
+            this.a = a;
+        }
+        public void setB(int b) {
+            this.b = b;
+        }
+        public void setC(int c) {
+        this.c = c;
+        }
+        public void setD(int d) {
+        this.d= d;
+        }
+        public void setE(int e) {
+        this.e = e;
+        }
+        public void setF(int F) {
+        this.f = f;
+        }
+        public void setG(int g) {
+        this.g = g;
+        }
+        public void setH(int h) {
+        this.h = h;
+        }
+        public String toString() {
+            return  "(a, b) - (c, d) - (e, f) - (g, h) - (a, b)";
+        }
+    }
+}
