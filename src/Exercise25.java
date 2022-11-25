a274b2e372508ec338d543663fce3b421d846175
DavidNebot94 <informaticadavidnebot@gmail.com>
2022-11-25 09:42:43 +0100

    David Nebot apply changes

---
+++ src/Exercise25.java
@@ -0,0 +1,15 @@
+public class Exercise25 {
+    public class Point {
+        private int x;
+        private int y;
+        private int w;
+        private int v;
+        public Point(int a, int b, int c, int v) {
+            x = a;
+            y = b;
+            w = c;
+            v = 1;
+        }
+       }
+    }
+
